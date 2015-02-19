a8f4ae532cf9f02e71d15b6eb84e534f12c7fcc1
unknown <maheshkk@INMAHESKK.corporate.thoughtworks.com>
2015-02-19 22:34:34 +0530

    WC is completed for command line and only for single file without options

--- Wc.java
+++ Wc.java
@@ -1,20 +1,18 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+// import java.io.BufferedReader;
+// import java.io.FileReader;
+// import java.io.IOException;
+import java.io.*;
 
 public class Wc {
-	public static void main(String[] args) {
-		String fileData = "";
-
-		try{
-			BufferedReader br = new BufferedReader(new FileReader(args[0]));
-			String newLine;
-			while ((newLine = br.readLine()) != null){fileData += newLine+"\r\n";}
-		} catch(Exception err){
-			System.out.println("Error--------"+err);
-		}
-		WcLib count = new WcLib(fileData);
-		count.callAllCounts();
-		System.out.println("\t"+count.lineCount+"\t"+count.wordCount+"\t"+count.charCount+"\t"+args[0]);
+	public static void main(String[] args) throws IOException{
+		File file = new File(args[0]);
+		int fileLength = (int)file.length();
+		char[] fileData = new char[fileLength];		
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		br.read(fileData, 0, fileLength);
+		WcLib wc = new WcLib(new String(fileData));
+		wc.callAllCounts();
+		System.out.println(wc.lineCount+"\t"+wc.wordCount+"\t"+wc.charCount+"\t"+args[0]);
 	}
 }
